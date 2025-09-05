d3b08ae41e7ed56362e5404f41a1a5aba12cd861
mcchotaliya2005-jpg <mcchotaliya2005@gmail.com>
2025-09-05 10:59:09 +0530

    commit

---
+++ task -1/main (4).c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main()
+{
+ int num,rev=0;
+ 
+ printf("enter any num");
+ scanf("%d",&num);
+ 
+ while (num>0)
+ 
+ {
+     int rem = num%10;
+     rev = (rev*10)+rem;
+     num = num/10;
+     
+ }
+    printf("your reverse num is %d",rev);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    getch();    
+}
