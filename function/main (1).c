31bd7c3d20ce33d41ba64363c049ac544ee845e8
mcchotaliya2005-jpg <mcchotaliya2005@gmail.com>
2025-09-14 15:37:35 +0530

    commit

---
+++ function/main (1).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+   char ch[50];
+   printf("enter your name");
+   scanf("%s",&ch);
+   
+   printf(" your name is %s",ch);
+   
+    return 0;
+    
+}
