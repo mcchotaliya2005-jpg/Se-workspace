39712e72623d3695013a30e1448420298b816511
mcchotaliya2005-jpg <mcchotaliya2005@gmail.com>
2025-09-14 15:52:29 +0530

    commit

---
+++ function/main (2).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+   char ch;
+   
+   printf("enter your name");
+   scanf("%c",&ch);
+   
+   printf(" your name is %c",ch);
+   
+    return 0;
+    
+}
---
+++ function/main (3).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+   char ch[50];
+   
+   printf("enter your name");
+   scanf("%s",&ch);
+   
+   printf("%d",strlen(ch));
+   
+    return 0;
+    
+}
