61098f782e8263bf64e7068e55126072928816c3
mcchotaliya2005-jpg <mcchotaliya2005@gmail.com>
2025-12-18 10:32:09 +0530

    commit

---
+++ Assignment/Module 3/Lab Exercises/Array Sum and Average.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum = 0;
+    float average;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " integers:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum = sum + arr[i];
+    }
+
+    average = (float)sum / n;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average << endl;
+
+    return 0;
+}
